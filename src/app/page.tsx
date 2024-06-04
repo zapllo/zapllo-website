e7b90f142d11e0618d83b37f3923b0c0807c26c7
zapllo <shubhodeep@zapllo.com>
2024-06-04 15:35:14 +0530

    &apos;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    // <LoaderLayout>
+    <LoaderLayout>
       <main className="bg-[#] bg-[#05071E] mx-auto h-full z-10 overflow-hidden">
         <FloatingNavbar />
         <Image src='/mask.png' height={1000} className=" absolute overflow-hidden w-full " width={1000} alt="Background mask for zapllo automation" />
@@ -89,6 +89,6 @@ export default function Home() {
           <Footer />
         </div>
       </main>
-    // </LoaderLayout>
+    </LoaderLayout>
   );
 }
