dd5b8e1a6f96737262114e3f4a4b64fe938fb287
zapllo <shubhodeep@zapllo.com>
2024-06-06 15:02:41 +0530

    Services

--- src/components/globals/service.tsx
+++ src/components/globals/service.tsx
@@ -125,7 +125,7 @@ export default function Service() {
                         variants={cardVariants('left', 2.2)}
                     >
                         <div>
-                        <img src='/card/automation.jpg' className='w-full h-40 rounded-lg  object-cover rounded-b-none' />
+                            <img src='/card/automation.jpg' className='w-full h-40 rounded-lg  object-cover rounded-b-none' />
                             <div className=' flex -mt-10  ml-[80%]'>
                                 <div className='flex justify-center'>
                                     <SignalIcon imgSrc={imgSrc3} />
@@ -151,7 +151,7 @@ export default function Service() {
                         variants={cardVariants('right', 0.6)}
                     >
                         <div>
-                        <img src='/card/genai5.png'  className='w-full h-40 rounded-lg  object-cover rounded-b-none' />
+                            <img src='/card/genai5.png' className='w-full h-40 rounded-lg  object-cover rounded-b-none' />
                             <div className=' flex -mt-10  ml-[80%]'>
                                 <div className='flex justify-center'>
                                     <SignalIcon imgSrc={imgSrc4} />
@@ -174,7 +174,7 @@ export default function Service() {
                         variants={cardVariants('right', 1.8)}
                     >
                         <div>
-                        <img src='/card/team.png' className='w-full h-40 rounded-lg  object-cover rounded-b-none' />
+                            <img src='/card/team.png' className='w-full h-40 rounded-lg  object-cover rounded-b-none' />
                             <div className=' flex -mt-10  ml-[80%]'>
                                 <div className='flex justify-center'>
                                     <SignalIcon imgSrc={imgSrc5} />
@@ -195,12 +195,10 @@ export default function Service() {
                         variants={cardVariants('right', 2.8)}
                     >
                         <div>
-                        <img src='/card/automation2.png' className='w-full h-40 rounded-lg  object-cover rounded-b-none' />
+                            <img src='/card/automation2.png' className='w-full h-40 rounded-lg  object-cover rounded-b-none' />
                             <div className=' flex -mt-10  ml-[80%]'>
                                 <div className='flex justify-center'>
                                     <SignalIcon imgSrc={imgSrc6} />
-
-
                                 </div>
                             </div>
                         </div>
