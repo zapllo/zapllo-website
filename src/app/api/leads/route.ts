56e051451fc418dbd426db073905a0ff2e965e21
zapllo <shubhodeep@zapllo.com>
2024-06-06 23:26:42 +0530

    Contact Form Completed

--- src/app/api/leads/route.ts
+++ src/app/api/leads/route.ts
@@ -8,29 +8,41 @@ export const dynamic = 'force-dynamic'; // Ensures the route is always dynamic
 export async function POST(request: NextRequest) {
     try {
         await connectDB();
+
         const { email, firstName, lastName, message, mobNo } = await request.json();
+
         if (!email) {
             return NextResponse.json({ error: 'Email is required' }, { status: 400 });
         }
+
         if (!firstName) {
             return NextResponse.json({ error: 'First Name is required' }, { status: 400 });
         }
+
         if (!lastName) {
             return NextResponse.json({ error: 'Last Name is required' }, { status: 400 });
         }
+
         if (!mobNo) {
             return NextResponse.json({ error: 'Mob No is required' }, { status: 400 });
         }
+
         if (!message) {
             return NextResponse.json({ error: 'Message is required' }, { status: 400 });
         }
+
         const existingSubscriber = await Lead.findOne({ email });
+
         if (existingSubscriber) {
             return NextResponse.json({ error: 'Email already subscribed' }, { status: 400 });
         }
-        const newSubscriber = new Lead({ email });
+
+        const newSubscriber = new Lead({ email, firstName, lastName, mobNo, message });
+
         await newSubscriber.save();
+
         return NextResponse.json({ message: 'Lead Captured successful!' }, { status: 201 });
+
     } catch (error) {
         console.error(error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
