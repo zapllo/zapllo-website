75ae627a739d1f513bcf92476b47133ce76747ea
sulphurninja <sulphurninja@gmail.com>
2024-10-19 01:20:56 +0530

    Build

--- src/app/(main)/(pages)/attendance/my-attendance/page.tsx
+++ src/app/(main)/(pages)/attendance/my-attendance/page.tsx
@@ -109,23 +109,23 @@ export default function MyAttendance() {
     const [isStartPickerOpen, setIsStartPickerOpen] = useState(false); // For triggering the start date picker
     const [isEndPickerOpen, setIsEndPickerOpen] = useState(false); // For triggering the end date picker
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            // Dynamically import Leaflet inside useEffect
-            import('leaflet').then((L) => {
-                // Dynamically set Leaflet icon options only after the window object is available
-                delete (L.Icon.Default.prototype as any)._getIconUrl;
-                L.Icon.Default.mergeOptions({
-                    iconRetinaUrl:
-                        'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-                    iconUrl:
-                        'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-                    shadowUrl:
-                        'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-                });
-            });
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined') {
+    //         // Dynamically import Leaflet inside useEffect
+    //         import('leaflet').then((L) => {
+    //             // Dynamically set Leaflet icon options only after the window object is available
+    //             delete (L.Icon.Default.prototype as any)._getIconUrl;
+    //             L.Icon.Default.mergeOptions({
+    //                 iconRetinaUrl:
+    //                     'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    //                 iconUrl:
+    //                     'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    //                 shadowUrl:
+    //                     'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    //             });
+    //         });
+    //     }
+    // }, []);
 
 
     useEffect(() => {
