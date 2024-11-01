3af70811f7a67e2eec14081874c7723dcfe5916f
sulphurninja <sulphurninja@gmail.com>
2024-11-01 16:30:51 +0530

    Webhook check for ui changes

--- src/app/(main)/(pages)/layout.tsx
+++ src/app/(main)/(pages)/layout.tsx
@@ -9,7 +9,7 @@ type Props = { children: React.ReactNode }
 const Layout = ({ children }: Props) => {
 
     return (
-        <div className=' bg-[#04061e]  scrollbar-hide h-screen w-full  border-muted-foreground/20  overflow-hidden '>
+        <div className=' bg-[#04061e]  scrollbar-hide h-screen w-full  border-muted-foreground/30  overflow-hidden '>
             <div className='w-full h-screen overflow-hidden'>
                 {children}
             </div>
