b9e9a5a8cc993e89ee6f38232d1d32555e5c8643
sulphurninja <sulphurninja@gmail.com>
2024-12-15 23:12:26 +0530

    Sentry removed

---
+++ src/types/enums.ts
@@ -0,0 +1,13 @@
+export enum RepeatType {
+    Weekly = "Weekly",
+    Monthly = "Monthly",
+    Daily = "Daily",
+}
+
+// Define the Status enum
+export enum Status {
+    Pending = "Pending",
+    Completed = "Completed",
+    InProgress = "In Progress",
+    Reopen = "Reopen",
+}
